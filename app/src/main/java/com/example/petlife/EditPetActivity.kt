6e133b305602d80f8ee499be2bb42fb4980552fc
LVSigoli <lucasviniciussigoli@gmail.com>
2024-10-17 19:25:52 -0300

    feat: Add Pet data Class

---
+++ app/src/main/java/com/example/petlife/EditPetActivity.kt
@@ -0,0 +1,4 @@
+package com.example.petlife
+
+class EditPetActivity {
+}
---
+++ app/src/main/java/com/example/petlife/model/Pet.kt
@@ -0,0 +1,20 @@
+package com.example.petlife.model
+
+enum class Species {
+    DOG, CAT
+}
+
+enum class Sizes{ SMALL, MEDIUM, BIG}
+
+
+data class Pet (var nome: String,
+    var birthDate: String,
+    var species: Species,
+    var color: String,
+    var size: Sizes,
+    var lastVeterinarianSeen: String,
+    var lastPetShopDate: String,
+    var lastVacinartionDate: String
+)
+
+
