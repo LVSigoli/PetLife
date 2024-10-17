1f8c011b7a18cb50284fdf24c89c6033f7d11ed8
LVSigoli <lucasviniciussigoli@gmail.com>
2024-10-17 19:33:01 -0300

    feat: Add default Pet to mainActivity

--- app/src/main/java/com/example/petlife/MainActivity.kt
+++ app/src/main/java/com/example/petlife/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.petlife
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.petlife.databinding.ActivityMainBinding
+import com.example.petlife.model.Pet
+import com.example.petlife.model.Sizes
+import com.example.petlife.model.Species
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+    private lateinit var binding: ActivityMainBinding
+    private var pet = Pet(
+        "Rex", "01/01/2020", Species.DOG, "Marrom", Sizes.MEDIUM,
+        "10/10/2023", "12/09/2023", "05/05/2023"
+    )
+
+
+
 }
+
+
