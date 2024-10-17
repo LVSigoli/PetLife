babe50778eb79aeb0145c49accb993639926f790
LVSigoli <lucasviniciussigoli@gmail.com>
2024-10-17 19:59:15 -0300

    feat: Add editPetBinding

--- app/src/main/java/com/example/petlife/MainActivity.kt
+++ app/src/main/java/com/example/petlife/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.petlife
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.petlife.databinding.ActivityMainBinding
 import com.example.petlife.model.Pet
@@ -15,35 +18,47 @@ class MainActivity : AppCompatActivity() {
         "10/10/2023", "12/09/2023", "05/05/2023"
     )
 
-    override fun onCreate(saveinstancestate: Bundle?){
-        super.onCreate(saveinstancestate)
+
+    private lateinit var editPetLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         displayPetData()
 
+        editPetLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+
+                val updatedPetName = result.data?.getStringExtra("updatedName")
+                updatedPetName?.let {
+                    pet.name = it
+                    displayPetData()
+                }
+            }
+        }
+
+
+        binding.btnEditPet.setOnClickListener { editPetBinding() }
     }
 
-    private fun displayPetData(){
+    private fun editPetBinding() {
+        val intent = Intent(this, EditPetActivity::class.java)
+        intent.putExtra("name", pet.name)
+
+        editPetLauncher.launch(intent)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun displayPetData() {
         binding.tvNome.text = "Nome: " + pet.name
-
-        binding.tvDataNascimento.text = "Data de nascimento: "+ pet.birthDate
-
+        binding.tvDataNascimento.text = "Data de nascimento: " + pet.birthDate
         binding.tvTipo.text = "Espécie: " + pet.species.toString()
-
         binding.tvCor.text = "Cor: " + pet.color
-
-        binding.tvPorte.text = "Porte: "+ pet.size.toString()
-
-        binding.tvUltimaVet.text ="Ultima ida ao veterinário: "+  pet.lastVeterinarianSeen
-
-        binding.tvUltimaPetshop.text ="Ultima ida ao petshop: "+ pet.lastPetShopDate
-
-        binding.tvUltimaVacinacao.text = "Ultima vacinação em: "+ pet.lastVacinationDate
+        binding.tvPorte.text = "Porte: " + pet.size.toString()
+        binding.tvUltimaVet.text = "Ultima ida ao veterinário: " + pet.lastVeterinarianSeen
+        binding.tvUltimaPetshop.text = "Ultima ida ao petshop: " + pet.lastPetShopDate
+        binding.tvUltimaVacinacao.text = "Ultima vacinação em: " + pet.lastVacinationDate
     }
-
-
-
 }
-
-
