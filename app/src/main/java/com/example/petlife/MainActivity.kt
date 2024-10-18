6b8f817eb75a9432adf54a3c20b76bc91d749fe5
LVSigoli <lucasviniciussigoli@gmail.com>
2024-10-17 21:11:34 -0300

    feat: Add edit color activity

--- app/src/main/java/com/example/petlife/MainActivity.kt
+++ app/src/main/java/com/example/petlife/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     )
 
     private lateinit var editPetLauncher: ActivityResultLauncher<Intent>
+    private lateinit var editPetColorLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +28,9 @@ class MainActivity : AppCompatActivity() {
 
         displayPetData()
 
+        // Launcher para editar os dados do pet
         editPetLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == RESULT_OK) {
-
                 val updatedPet = result.data?.getSerializableExtra("pet") as? Pet
                 updatedPet?.let {
                     pet = it
@@ -38,25 +39,42 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // Launcher para editar a cor do pet
+        editPetColorLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val updatedPet = result.data?.getSerializableExtra("pet") as? Pet
+                updatedPet?.let {
+                    pet = it
+                    displayPetData() // Atualiza a exibição com os dados do pet
+                }
+            }
+        }
+
         binding.btnEditPet.setOnClickListener { editPetBinding() }
+        binding.btnEditPetColor.setOnClickListener { editPetColorBinding() }
     }
 
     private fun editPetBinding() {
         val intent = Intent(this, EditPetActivity::class.java)
         intent.putExtra("pet", pet)
-
         editPetLauncher.launch(intent)
+    }
+
+    private fun editPetColorBinding() {
+        val intent = Intent(this, EditPetColorActivity::class.java)
+        intent.putExtra("pet", pet)
+        editPetColorLauncher.launch(intent)  // Inicia a EditPetColorActivity
     }
 
     @SuppressLint("SetTextI18n")
     private fun displayPetData() {
-        binding.tvNome.text = "Nome: " + pet.name
-        binding.tvDataNascimento.text = "Data de nascimento: " + pet.birthDate
-        binding.tvTipo.text = "Espécie: " + pet.species.toString()
-        binding.tvCor.text = "Cor: " + pet.color
-        binding.tvPorte.text = "Porte: " + pet.size.toString()
-        binding.tvUltimaVet.text = "Ultima ida ao veterinário: " + pet.lastVeterinarianSeen
-        binding.tvUltimaPetshop.text = "Ultima ida ao petshop: " + pet.lastPetShopDate
-        binding.tvUltimaVacinacao.text = "Ultima vacinação em: " + pet.lastVacinationDate
+        binding.tvNome.text = "Nome: ${pet.name}"
+        binding.tvDataNascimento.text = "Data de nascimento: ${pet.birthDate}"
+        binding.tvTipo.text = "Espécie: ${pet.species}"
+        binding.tvCor.text = "Cor: ${pet.color}"
+        binding.tvPorte.text = "Porte: ${pet.size}"
+        binding.tvUltimaVet.text = "Última ida ao veterinário: ${pet.lastVeterinarianSeen}"
+        binding.tvUltimaPetshop.text = "Última ida ao petshop: ${pet.lastPetShopDate}"
+        binding.tvUltimaVacinacao.text = "Última vacinação em: ${pet.lastVacinationDate}"
     }
 }
