482c6b88e9a614a83cf03ab030cd674fb936f769
LVSigoli <lucasviniciussigoli@gmail.com>
2024-10-20 20:42:39 -0300

    refactor: Remove unused screens

--- app/src/main/java/com/example/petlife/EditiVacinationActivity.kt
+++ app/src/main/java/com/example/petlife/EditiVacinationActivity.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.petlife.databinding.ActivityEditPetBirthBinding
+
 import com.example.petlife.databinding.ActivityEditPetVacBinding
 
 import com.example.petlife.model.Pet
